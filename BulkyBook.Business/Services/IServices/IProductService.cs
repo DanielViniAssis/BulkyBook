c8b67f81a37eba4cfcceae4d68bc09c2846db112
DanielViniAssis <dvs.daniel11@gmail.com>
2026-09-01 18:10:54 -0300

    feat: add product controller, service, Iservice and views

---
+++ BulkyBook.Business/Services/IServices/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Business.Services.IServices
+{
+    public interface IProductService
+    {
+        Task <Product?> GetProductByIdAsync(int id);
+        Task<IEnumerable<Product>> GetAllProductsAsync();
+        Task <Product> CreateProductAsync(Product product);
+        Task UpdateProductAsync(Product product);
+        Task DeleteProductAsync(int id);
+    }
+}
---
+++ BulkyBook.Business/Services/ProductService.cs
@@ -0,0 +1,54 @@
+﻿using BulkyBook.Business.Services.IServices;
+using BulkyBook.DataAccess;
+using BulkyBook.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBook.Business.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly ApplicationDbContext _context;
+        public ProductService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Product>> GetAllProductsAsync()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        public async Task<Product?> GetProductByIdAsync(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+
+        public async Task<Product> CreateProductAsync(Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task DeleteProductAsync(int id)
+        {
+            var product = _context.Products.Find(id);
+
+            if (product == null)
+            {
+                throw new KeyNotFoundException($"Product with id {id} not found.");
+            }
+
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+        }
+   
+        public async Task UpdateProductAsync(Product product)
+        {
+            _context.Products.Update(product);
+            await _context.SaveChangesAsync();
+        }
+
+        
+    }
+}
