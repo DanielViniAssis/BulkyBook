214d245d6629c439eefd2686649333e60fb711ff
DanielViniAssis <dvs.daniel11@gmail.com>
2026-09-01 17:46:51 -0300

    feat: add product model and table in the database

---
+++ BulkyBook.Models/Product.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.InteropServices;
+
+namespace BulkyBook.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id  { get; set; }
+
+        [Required]
+        public string Title { get; set; } = String.Empty;
+
+        public string Description { get; set; } = String.Empty;
+
+        [Required]
+        public string ISBN { get; set; } = String.Empty;
+
+        [Required]
+        public string Author { get; set; } = String.Empty;
+
+        [Required]
+        [DisplayName("List Price")]
+        [Range(1,1000)]
+        public double ListPrice { get; set; }
+
+        [Required]
+        [DisplayName("Price for 1-50")]
+        [Range(1, 1000)]
+        public double Price { get; set; }
+
+        [Required]
+        [DisplayName("Price for 50+")]
+        [Range(1, 1000)]
+        public double Price50 { get; set; }
+
+        [Required]
+        [DisplayName("Price for 100+")]
+        [Range(1, 1000)]
+        public double Price100 { get; set; }
+
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+
+        [ValidateNever]
+        [DisplayName("Product Image")]
+        public string? ImageUrl { get; set; }
+
+    }
+}
